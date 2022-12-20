43f0afa3b25dd3f6679e71e7e906e6f16e7ce9bf
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2022-12-20 11:47:47 +0700

    Create playRound and game function
    - playRound need player and computer choice and compare those choices
    - game will loop playRound 5 times and ask input from user

--- script.js
+++ script.js
@@ -16,4 +16,74 @@ const getComputerChoice = () => {
     return computerChoice
 }
 
-console.log(getComputerChoice())
+const playRound = (playerSelection, computerSelection) => {
+    let matchResult = ``;
+    let matchMessage = ``;
+    let matchStatus = ``;
+
+    if (playerSelection.toLowerCase() === "rock") {
+        switch (computerSelection.toLowerCase()) {
+            case "rock":
+                matchStatus = "Draw";
+                break;
+            case "paper":
+                matchStatus = "Lose";
+                break;
+            case "scissors":
+                matchStatus = "Win";
+                break;
+        }
+    } else if (playerSelection.toLowerCase() === "paper") {
+        switch (computerSelection.toLowerCase()) {
+            case "rock":
+                matchStatus = "Win";
+                break;
+            case "paper":
+                matchStatus = "Draw";
+                break;
+            case "scissors":
+                matchStatus = "Lose";
+                break;
+        }
+    } else {
+        switch (computerSelection.toLowerCase()) {
+            case "rock":
+                matchStatus = "Lose";
+                break;
+            case "paper":
+                matchStatus = "Win";
+                break;
+            case "scissors":
+                matchStatus = "Draw";
+                break;
+        }
+    }
+    
+    switch (matchStatus.toLowerCase()) {
+        case 'win':
+            matchMessage = `${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}`;
+            break;
+        case 'lose':
+            matchMessage = `${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`;
+            break;
+        case 'draw':
+            matchMessage = "Same selection";
+            break;
+    }
+
+    matchResult = `You ${matchStatus}! ${matchMessage}`;
+
+    return matchResult;
+}
+
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Enter your answer");
+        const computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("\n\n");
+    }
+}
+
+game();
